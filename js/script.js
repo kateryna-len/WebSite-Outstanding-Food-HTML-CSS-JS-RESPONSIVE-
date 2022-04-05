1dd06d55ea440c7a9265da64312d2c089c35b786
kateryna-len <lenkaterynna057@gmail.com>
2022-04-05 11:21:15 +0300

    create section blog and footer

--- js/script.js
+++ js/script.js
@@ -86,3 +86,30 @@ let swiper3 = new Swiper(".menu-slider", {
   },
 
 });
+
+
+let swiper4 = new Swiper(".blog-slide", {
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  autoHeight: true,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+
+    },
+    700: {
+      slidesPerView: 2,
+
+    },
+    1000: {
+      slidesPerView: 3,
+
+    },
+  },
+});
